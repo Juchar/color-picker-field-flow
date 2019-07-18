f53726dfc8886d18cdd1c40c6a055fa51d235cbb
Juchar <Julien.Charpenel@Gmail.com>
2019-07-18 07:04:49 +0200

    Add history property

--- src/main/java/com/github/juchar/colorpicker/ColorPickerFieldBase.java
+++ src/main/java/com/github/juchar/colorpicker/ColorPickerFieldBase.java
@@ -8,6 +8,12 @@ public interface ColorPickerFieldBase<T> extends ColorPickerBase<T> {
   PropertyDescriptor<Boolean, Boolean> SHOW_CHANGE_FORMAT_BUTTON_PROPERTY
       = PropertyDescriptors.propertyWithDefault("showChangeFormatButton", false);
 
+  PropertyDescriptor<Boolean, Boolean> ENABLE_HISTORY_PROPERTY
+      = PropertyDescriptors.propertyWithDefault("enableHistory", false);
+
+  PropertyDescriptor<Integer, Integer> MAX_HISTORY_PROPERTY
+      = PropertyDescriptors.propertyWithDefault("maxHistory", 10);
+
   /**
    * Check if the button to change the color format inside the input field is visible.
    *
@@ -18,11 +24,55 @@ public interface ColorPickerFieldBase<T> extends ColorPickerBase<T> {
   }
 
   /**
-   * Speciy if the button to change the color format inside the input field is visible.
+   * Specify if the button to change the color format inside the input field is visible.
    *
    * @param visible {@code true} if the button should be visible, {@code false} else
    */
   default void setChangeFormatButtonVisible(boolean visible) {
     SHOW_CHANGE_FORMAT_BUTTON_PROPERTY.set(this, visible);
+  }
+
+  /**
+   * Check if history is enabled for picked colors.
+   *
+   * @return {@code true} if the history is enabled, {@code false} else
+   *
+   * @see #getMaxHistory()
+   */
+  default boolean isHistoryEnabled() {
+    return ENABLE_HISTORY_PROPERTY.get(this);
+  }
+
+  /**
+   * Enable or disable the history for picked colors.
+   *
+   * @param enabled {@code true} if the history should be enabled, {@code false} else
+   *
+   * @see #setMaxHistory(int)
+   */
+  default void setHistoryEnabled(boolean enabled) {
+    ENABLE_HISTORY_PROPERTY.set(this, enabled);
+  }
+
+  /**
+   * Check how many colors should be in the history of picked colors.
+   *
+   * @return The maximum amount of colors in the history
+   *
+   * @see #isHistoryEnabled()
+   */
+  default int getMaxHistory() {
+    return MAX_HISTORY_PROPERTY.get(this);
+  }
+
+  /**
+   * Specify how many colors should be in the history of picked colors.
+   *
+   * @param maxHistory The maximum amount of colors in the history
+   *
+   * @see #setHistoryEnabled(boolean)
+   */
+  default void setMaxHistory(int maxHistory) {
+    MAX_HISTORY_PROPERTY.set(this, maxHistory);
   }
 }
